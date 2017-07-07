bca4c22ab75651b06a8e1cccbb017aacfa2d4fc0
ozeki <hhdfgg@users.noreply.github.com>
2017-07-07 15:55:39 +0900

    シーン切り替えの実装

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Game manager.
+/// Oキーでゲームオーバー
+/// Cキーでクリア
+/// 両方のシーン切り替えが同時に発生しないようにする
+/// </summary>
+
+public class GameManager : MonoBehaviour {
+	// 次のシーンを指定する変数、空文字のときは何もしない
+	public static string NextScene = "";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Oキー
+		if (Input.GetKeyDown (KeyCode.O)) {
+			NextScene = "GameOver";
+		}
+		// Cキー
+		else if (Input.GetKeyDown (KeyCode.C)){
+			NextScene = "Clear";
+		}
+
+		// シーン切り替え処理
+		if (NextScene.Length > 0){
+			SceneManager.LoadSceneAsync (NextScene, LoadSceneMode.Additive);
+			NextScene = "";
+			enabled = false;
+		}
+	}
+}
