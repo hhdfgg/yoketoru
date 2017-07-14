d4cd949651fbcec1305ea071bafbba250979a84c
ozeki <hhdfgg@users.noreply.github.com>
2017-07-14 15:20:32 +0900

    hanekaeriをインポート

---
+++ Assets/Hanekaeri/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float MAX_SPEED = 6f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (Input.mousePosition);
+
+		Vector3 mpos = Input.mousePosition;
+		mpos.z = transform.position.z - Camera.main.transform.position.z;
+		Vector3 target = Camera.main.ScreenToWorldPoint (mpos);
+
+		// 自分からマウスへのベクトル(一発で目的地に到達する)
+		//Vector3 dir = traget - transform.position;
+		// dirの長さが最大速度より長かったら、長さを最大速度にする
+		// そうでなければ、dirをそのまま使う
+
+		// Unityには、そのための機能がある！！
+		Vector3 newpos = Vector3.MoveTowards (
+			transform.position,
+			target,
+			MAX_SPEED * Time.deltaTime);	// Time.deltaTimeで1回分の経過時間
+
+		transform.position = newpos;
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.CompareTag("Teki")){
+			Destroy (gameObject);
+			GameManager.NextScene = "GameOver";
+		}
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,8 +11,22 @@ using UnityEngine.SceneManagement;
 /// </summary>
 
 public class GameManager : MonoBehaviour {
+	// 次のシーンを記録する
+	private static string _nextScene = "";
+
 	// 次のシーンを指定する変数、空文字のときは何もしない
-	public static string NextScene = "";
+	public static string NextScene {
+		get { return _nextScene; }
+		set { 
+			// 現在clearが設定されていないか、
+			// あるいは、新しく空文字を設定したいときに設定を許可する
+			if (    (_nextScene != "clear")
+				||  (value == "")) 
+			{
+				_nextScene = value;
+			}
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -31,6 +45,7 @@ public class GameManager : MonoBehaviour {
 		// Cキー
 		else if (Input.GetKeyDown (KeyCode.C)){
 			NextScene = "Clear";
+			NextScene = "GameOver";
 		}
 		// Aキー
 		else if (Input.GetKey (KeyCode.A)){
